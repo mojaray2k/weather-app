8d44a350bbd80174debe40d607dd3e0b55a00e4b
Amen Ra <ara@envistacorp.com>
2018-03-16 03:50:23 -0400

    refactor: Added error handling and encoding address
    I used the encodeURIComponent Object the handle spaces in the address request and have error
    messages if the address cannot be found, if we cannot connect to google servers, and if we need ot
    clear the cache.

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+//console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=5210%203rd%20St.%20NE%20Washington%20D.C.',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if (body.status === 'OVER_QUERY_LIMIT'){
+        console.log('Restart your terminal or pres ctr+shift+r in your web browser')
+    }else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
